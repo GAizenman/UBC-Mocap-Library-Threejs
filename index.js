22d43a21c78de75ee575936c49f12c3471465757
Gabi Aizenman <lucky3gabi@gmail.com>
2023-02-19 23:59:50 -0800

    removed extra animation selectors

--- index.js
+++ index.js
@@ -3,11 +3,4 @@ import { init as animationSelector } from "./src/animationSelector.js";
 
 const path =  "../assets/gltf/Female_Default.glb"
 animationSelector(path);
-animationSelector(path);
-animationSelector(path);
-animationSelector(path);
-animationSelector(path);
-animationSelector(path);
-animationSelector(path);
-animationSelector(path);
 // loadCharacter(path);
